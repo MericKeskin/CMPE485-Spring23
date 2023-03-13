116769b36eec6ec5a6fdfcd426154cb78193186a
Meric Keskin <mericyk@gmail.com>
2023-03-13 21:21:19 +0300

    Step 11
    Updated the script for cube 7 to give it rotated force by clicking WASD on keyboard

--- HW#1/Assets/Scripts/SevenScript.cs
+++ HW#1/Assets/Scripts/SevenScript.cs
@@ -15,6 +15,17 @@ public class SevenScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, -500*Time.deltaTime, 2000*Time.deltaTime);
+        if (Input.GetKey("w")) {
+            rb.AddForce(0, 0, 2000*Time.deltaTime);
+        }
+        if (Input.GetKey("a")) {
+            rb.AddForce(-2000*Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s")) {
+            rb.AddForce(0, 0, -2000*Time.deltaTime);
+        }
+        if (Input.GetKey("d")) {
+            rb.AddForce(2000*Time.deltaTime, 0, 0);
+        }
     }
 }
