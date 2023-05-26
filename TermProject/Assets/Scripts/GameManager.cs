ce8d52790f4df124fb1cc42b47f942f2554ed419
Meric Keskin <mericyk@gmail.com>
2023-05-26 19:35:09 +0300

    First Commit of the Term Project
    This is a huge commit because I mistakingly commit my changes in a totally different repo. Realising now, I will start to commit my term project properly.
    Created the base of the game, added player, platforms, manually designed 3D trail effect, props and terrains. Added the main track for first level. Added game manager and audio manager for managing the essential game states.

---
+++ TermProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject startPlatform;
+    [SerializeField]
+    private GameObject dancingLine;
+    [HideInInspector]
+    public string gameState;
+    private AudioManager audioManager;
+    private bool audioOn;
+
+    IEnumerator LowerStartPlatform()
+    {
+        while (startPlatform.transform.position.y > -1.5) {
+            startPlatform.transform.position -= new Vector3(0, 0.05f, 0);
+            dancingLine.transform.position -= new Vector3(0, 0.05f, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+        gameState = "ready";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameState = "prep";
+        StartCoroutine(LowerStartPlatform());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameState == "ready" && Input.GetKeyDown(KeyCode.Space)) {
+            gameState = "start";
+        }
+    }
+}
---
+++ TermProject/Assets/Scripts/LineScript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineScript : MonoBehaviour
+{
+
+    private GameManager gameManager;
+    // private Rigidbody rb;
+    public float rate;
+    public float lineSpeed;
+    private float x_speed;
+    private float z_speed;
+    private float temp;
+    private Vector3 movement;
+    private Vector3 trailPosition;
+    private bool tap = false;
+    private bool firstTrail = false;
+    [SerializeField] private GameObject trailPrefab;
+
+    void Turn()
+    {
+        temp = x_speed;
+        x_speed = z_speed;
+        z_speed = temp;
+    }
+
+    void TrailLine()
+    {
+        trailPosition = transform.position;
+        GameObject trail = Instantiate(trailPrefab, trailPosition, transform.rotation);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        gameManager = FindObjectOfType<GameManager>();
+        // rb = gameObject.GetComponent<Rigidbody>();
+        x_speed = 0;
+        z_speed = lineSpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.gameState == "start" && Input.GetKeyDown(KeyCode.Space)) {
+            tap = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (gameManager.gameState == "ready" && !firstTrail) {
+            TrailLine();
+            firstTrail = true;
+        }
+        if (gameManager.gameState == "start") {
+            if (tap) {
+                Turn();
+                TrailLine();
+                tap = false;
+            }
+            movement = new Vector3(x_speed, 0, z_speed);
+            transform.position += movement;
+            if ((transform.position-trailPosition).magnitude > trailPrefab.transform.localScale.magnitude/2) {
+                TrailLine();
+            }
+        }
+    }
+}
